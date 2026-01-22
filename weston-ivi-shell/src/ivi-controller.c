f43167ac686ab2859a30553465d45af711d7e6cb
Domenico Nicita <domenico.nicita@artgroup-spa.com>
2026-01-22 14:46:33 +0100

    update: moving input-policy module load right after the input-module.

--- weston-ivi-shell/src/ivi-controller.c
+++ weston-ivi-shell/src/ivi-controller.c
@@ -2251,7 +2251,7 @@ static int load_art_input_policy_module(struct ivishell *shell)
 
     if (weston_config_section_get_string(section, "ivi-input-policy-module",
                                          &input_policy_module, NULL) < 0) {
-        /* input policy shall follow weston default */
+        /* input policy shall follow weston default, that is all the viewable region is considered input */
         weston_log("ivi-controller: No ivi-input-policy-module set\n");
         return_code = 0;
     } else {
@@ -2317,6 +2317,11 @@ wet_module_init(struct weston_compositor *compositor,
         return -1;
     }
 
+    /* Load input policy */
+    if (load_art_input_policy_module(shell) < 0) {
+        weston_log("ivi-controller: art-input-policy module not loaded\n");
+    }
+
     if (shell->bkgnd_surface_id && shell->ivi_client_name) {
         loop = wl_display_get_event_loop(compositor->wl_display);
         wl_event_loop_add_idle(loop, launch_client_process, shell);
@@ -2324,11 +2329,6 @@ wet_module_init(struct weston_compositor *compositor,
 
     if (load_id_agent_module(shell) < 0) {
         weston_log("ivi-controller: id-agent module not loaded\n");
-    }
-
-    /* Load Art Specific Modules, to setup input policy */
-    if (load_art_input_policy_module(shell) < 0) {
-        weston_log("ivi-controller: art-input-policy module not loaded\n");
     }
 
     /* add ivi-shell destroy signal after loading input
